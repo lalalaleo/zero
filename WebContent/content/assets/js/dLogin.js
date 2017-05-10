749dac82baaff8cb04f2197d4fcb14d40dd656ac
Leo Hu <lalalaleo.hu@hotmail.com>
2017-05-10 13:33:07 +0800

    桌面端页面基本完成

--- WebContent/content/assets/js/dLogin.js
+++ WebContent/content/assets/js/dLogin.js
@@ -1,17 +1,15 @@
 function submit(){
-    $.ajax({
-            url: "./content/assets/json/test_message_students.json",
-                type: "get",
-                data: {
-                	"claId": classTools.checked
-                	},
-                dataType: "JSON",
-                success: function(data) {
-                                studentTools.setList(data);
-                                studentTools.loadPage();
-                                studentTools.initPage();
-                            },
-                error: function() {alert("error");}
-        });
-    window.location.href="./dTeacherMessage.html";
+    // $.ajax({
+    //         url: "",
+    //             type: "get",
+    //             data: {
+    //             	"username": "123",
+    //                 "password": "123"
+    //             	},
+    //             dataType: "JSON",
+    //             success: function(data) {
+    //             },
+    //             error: function() {alert("error");}
+    //     });
+        window.location.href="./dTeacherMessage.html";
 }
