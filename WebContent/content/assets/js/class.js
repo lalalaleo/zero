0b4bea04afef989db66f8ed01f9c1b3b9094f6df
Leo Hu <lalalaleo.hu@hotmail.com>
2017-05-11 21:56:25 +0800

    课程页面上课时间数据处理

--- WebContent/content/assets/js/class.js
+++ WebContent/content/assets/js/class.js
@@ -40,8 +40,8 @@ function getUrlData(){
     else if((classid == null) || (classid == "")) goStatePage(3,2);
     else{
         $.ajax({
-            url: "findclazz.do",
-                type: "post",
+            url: "./content/assets/json/test_class.json",
+                type: "get",
                 data: {
                 	"claId": classid
                 	},
@@ -53,16 +53,42 @@ function getUrlData(){
                     goStatePage(3,1);
                 }
         });
+        // $.ajax({
+        //     url: "findclazz.do",
+        //         type: "post",
+        //         data: {
+        //         	"claId": classid
+        //         	},
+        //         dataType: "JSON",
+        //         success: function(data) {
+        //                         loadClassPage(data);
+        //                     },
+        //         error: function() {
+        //             goStatePage(3,1);
+        //         }
+        // });
     }
 }
 // 加载ClassPage
 function loadClassPage(data){
     var html = document.getElementById("classPage").innerHTML;
-    var source = html.replace(reg, function (node, key) { return {"className":data.class_name,"classInfo":data.class_info,"teacher_id":data.teacher_id,"teacherName":data.teacher_name,"count":data.count,"time":data.time}[key]; });
+    var source = html.replace(reg, function (node, key) { return {"className":data.class_name,"classInfo":data.class_info,"teacher_id":data.teacher_id,"teacherName":data.teacher_name,"time":getTime(data.time)}[key]; });
     $("#loading").remove();
     $(document.body).append(source);
     createNewPage();
     initPage();
+}
+// 时间处理
+function getTime(time){
+    var week = ["周一","周二","周三","周四","周五","周六","周日"];
+    var startList = ["8:00","8:50","9:50","10:40","11:30","13:30","14:20","15:20","16:10","18:30","19:20","20:10"];
+    var endList = ["8:45","9:35","10:35","11:25","12:15","14:15","15:05","16:05","16:55","19:15","20:05","20:55"];
+    var data_week = time.substr(0,time.indexOf(":"));
+    var data_time = time.substr(time.indexOf(":")+1);
+    var x = data_time.indexOf("-");
+    var start = parseInt(data_time.substring(0,x)) - 1;
+    var end = parseInt(data_time .substring(x+1)) - 1;
+    return week[parseInt(data_week)-1]+" "+startList[start] + '-' + endList[end];
 }
 // 初始化Page
 function initPage(){
