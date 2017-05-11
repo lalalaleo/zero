5624282a935744cc72361f0839bdc23037dd96a5
Kindred <hznathan@gmail.com>
2017-05-11 11:29:18 +0800

    整合鸡的页面
    补充了历史消息的页面

---
+++ WebContent/content/assets/js/dLogin.js
@@ -0,0 +1,15 @@
+function submit(){
+    // $.ajax({
+    //         url: "",
+    //             type: "get",
+    //             data: {
+    //             	"username": "123",
+    //                 "password": "123"
+    //             	},
+    //             dataType: "JSON",
+    //             success: function(data) {
+    //             },
+    //             error: function() {alert("error");}
+    //     });
+        window.location.href="./dTeacherMessage.html";
+}
